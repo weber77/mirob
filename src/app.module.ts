50d01f1454d4d75f22ded47e1cc4d5717ea8f29d
nthadrien <91905798+nthadrien@users.noreply.github.com>
2022-10-06 08:51:36 +0100

    likes added

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { BookmarksModule } from './bookmarks/bookmarks.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
+      port:+process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
